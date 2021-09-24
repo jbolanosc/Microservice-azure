b36a3352679ca5433b75f94b5bcb4454bb888d99
Josue Bolanos <josue.carit@outlook.com>
2021-09-24 12:00:38 -0600

    Azure devops test

---
+++ MicroserviceCourse.Api.Search/Services/CustomerService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using MicroserviceCourse.Api.Search.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace MicroserviceCourse.Api.Search.Services
+{
+    public class CustomerService : ICustumerService
+    {
+        private readonly IHttpClientFactory _http;
+        private readonly ILogger _logger;
+
+        public CustomerService(IHttpClientFactory http, ILogger logger)
+        {
+            _http = http;
+            _logger = logger;
+        }
+        public async Task<(bool isSuccess, dynamic Customer, string errorMessage)> GetCustomerAsync(int id)
+        {
+            try
+            {
+                var client = _http.CreateClient("CustomerService");
+                var response = await client.GetAsync($"api/customer/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsByteArrayAsync();
+                    var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+                    var result = JsonSerializer.Deserialize<dynamic>(content, options);
+
+                    return (true, result, null);
+                }
+
+                return (false, null, response.ReasonPhrase);
+            }
+            catch (Exception ex)
+            {
+                _logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+
+        private object JsonSerializerOptions()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MicroserviceCourse.Api.Search/Services/ProductService.cs
@@ -0,0 +1,47 @@
+﻿using MicroserviceCourse.Api.Search.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace MicroserviceCourse.Api.Search.Services
+{
+    public class ProductService : Interfaces.IProductService
+    {
+        private readonly IHttpClientFactory _http;
+        private readonly Logger<Interfaces.IProductService> _logger;
+        private readonly Interfaces.IProductService _service;
+
+        public ProductService(Interfaces.IProductService service, IHttpClientFactory http, Logger<Interfaces.IProductService> logger)
+        {
+            _http = http;
+            _logger = logger;
+            _service = service;
+        }
+        public async Task<(bool isSuccess, IEnumerable<Product> products, string errorMessage)> GetProductsAsync()
+        {
+            try
+            {
+                var client = _http.CreateClient("ProductService");
+                var response = await client.GetAsync("api/products");
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsByteArrayAsync();
+                    var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+                    var result = JsonSerializer.Deserialize<IEnumerable<Product>>(content, options);
+                    return (true, result, null);
+                }
+
+                return (false, null, response.ReasonPhrase);
+            }
+            catch (Exception ex)
+            {
+                _logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+    }
+}
